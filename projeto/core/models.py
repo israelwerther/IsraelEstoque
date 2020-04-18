2f61a13d3ca42d81a38181386b7aaf24e5ab7786
israel <israelwerther48@outlook.com>
2020-04-18 17:32:12 -0300

    Sintax do model TimeStamped corrigida e app produto criada

--- projeto/core/models.py
+++ projeto/core/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 
-class TimeStampedModel(models.Model)
-    created = models.DateTimeField(
-        'criado em',
-        auto_now=True,
-        auto_now_add=False,
-    )
-    modified = models.DateTimeField(_("Modificado em"), 
-        auto_now=False, 
-        auto_now_add=True
-    )
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField("criado em", auto_now=True, auto_now_add=False)
+    modified = models.DateTimeField("Modificado em", auto_now=False, auto_now_add=True)
     
     class Meta:
         abstract = True
