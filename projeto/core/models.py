c6de7b9632d39bd9a93451810fb0329f8322b8ba
israel <israelwerther48@outlook.com>
2020-04-18 17:22:31 -0300

    Criação a app core

---
+++ projeto/core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class TimeStampedModel(models.Model)
+    created = models.DateTimeField(
+        'criado em',
+        auto_now=True,
+        auto_now_add=False,
+    )
+    modified = models.DateTimeField(_("Modificado em"), 
+        auto_now=False, 
+        auto_now_add=True
+    )
+    
+    class Meta:
+        abstract = True
