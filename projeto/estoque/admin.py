a9adf6b48d6ae37472cbe84c979f5e79be1119f1
israel <israelwerther48@outlook.com>
2020-04-19 07:19:07 -0300

    Tabularinline adcionado a estoque

--- projeto/estoque/admin.py
+++ projeto/estoque/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from .models import Estoque, EstoqueItens
 
 
+class EstoqueItensInline(admin.TabularInline):
+    model = EstoqueItens
+    extra = 0
+    
+    
+    
 @admin.register(Estoque)
 class EstoqueAdmin(admin.ModelAdmin):
-    list_display=('__str__', 'nf')
-    search_display=('nf',)
-    list_filter=('funcionario',)
-    date_hierarchy='created'
+    inlines = (EstoqueItensInline,) 
+    list_display = ('__str__', 'nf')
+    search_display = ('nf',)
+    list_filter = ('funcionario',)
+    date_hierarchy = 'created'
