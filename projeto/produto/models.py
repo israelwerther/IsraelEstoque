86ccb047e8d78f14af3aadf4fe4b650673e47938
israel <israelwerther48@outlook.com>
2020-04-19 16:23:00 -0300

    produto_detail adicionado

--- projeto/produto/models.py
+++ projeto/produto/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse_lazy
 
 class Produto(models.Model):
     importado      = models.BooleanField(default=False)
@@ -12,4 +13,8 @@ class Produto(models.Model):
         ordering=('produto',)
     
     def __str__(self):
-        return self.produto    
+        return self.produto  
+    
+    def get_absolute_url(self):
+        return reverse_lazy('produto:produto_detail', kwargs={"pk": self.pk})
+      
